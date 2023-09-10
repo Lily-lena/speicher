6115caa12cc939335f8c3414e8294f006f1bae5c
Lily <144317528+Lily-lena@users.noreply.github.com>
2023-09-10 11:40:32 +0200

    4er intervall beim Start-zurücksetzen

--- main.ts
+++ main.ts
@@ -5,9 +5,16 @@ if (Text_1 == 0) {
     basic.showNumber(Text_1)
     Text_1 += 1
     storage.putNumber(StorageSlots.s1, Text_1)
+} else if (Text_1 == 1 || Text_1 == 2) {
+    basic.setLedColor(0xffff00)
+    basic.showNumber(Text_1)
+    basic.pause(500)
+    Text_1 += 1
+    storage.putNumber(StorageSlots.s1, Text_1)
 } else {
     basic.setLedColor(0x00ff00)
     basic.showNumber(Text_1)
+    basic.pause(500)
     storage.removeNumber(StorageSlots.s1)
 }
 basic.forever(function () {
